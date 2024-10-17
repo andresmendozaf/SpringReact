02319d264328d2358a9e42c31c8f73ec1298a140
amendozaf <andres.mendozaf@gmail.com>
2024-10-17 17:00:46 -0300

    Sección 7 del curso - video 104

--- 06_users_app/src/UsersApp.jsx
+++ 06_users_app/src/UsersApp.jsx
@@ -1,8 +1,27 @@
+import { UserForm } from "./components/UserForm";
+import { UsersList } from "./components/UsersList";
+
 export const UsersApp = () => {
 
-    return(
-    <div className="container my-4">
-        <h2>Users App</h2>
-    
-    </div>);
+    const initialUsers = [
+        {
+            id: 1,
+            username: 'pepe',
+            password: '123456',
+            email: 'pepe@correo.com'
+        },
+    ];
+
+    return (
+        <div className="container my-4">
+            <h2>Users App</h2>
+            <div className="row">
+                <div className="col">
+                    <UserForm />
+                </div>
+                <div className="col">
+                    <UsersList users={initialUsers}/>
+                </div>
+            </div>
+        </div>);
 }
